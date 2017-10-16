54ca8ff57896db47afc23852a0760878e6dd62ad
Nurahmadie <nurahmadie@gmail.com>
2017-10-16 10:17:19 +0700

    Fix imports and stuff, add UUID for container name.

--- build/build.go
+++ build/build.go
@@ -2,13 +2,23 @@ package build
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"os"
+	"path"
 	"time"
 
 	"github.com/fudanchii/monocle/errors"
 
-	"github.com/moby/moby/api/types"
+	"github.com/docker/docker/api/types"
 	"github.com/moby/moby/client"
+	"github.com/satori/go.uuid"
 )
+
+func Name(buildFile string) string {
+	name := path.Base(path.Dir(buildFile))
+	return fmt.Sprintf("%s-%s", name, uuid.NewV5(uuid.NewV1(), buildFile))
+}
 
 func Start(buildName string, config *Build) {
 	cli, err := client.NewEnvClient()
@@ -23,12 +33,12 @@ func Start(buildName string, config *Build) {
 	rs, err := cli.ContainerCreate(ctx, cConfig, hConfig, nConfig, buildName)
 	errors.ErrCheck(err)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	logReader, err := cli.ContainerLogs(ctx, rs.ID, types.ContainerLogsOptions{})
 	errors.ErrCheck(err)
 
-	_, err := io.Copy(os.Stdout, logReader)
-	errors.AssertFalse(err != nil && err != io.EOF, err.Error)
+	_, err = io.Copy(os.Stdout, logReader)
+	errors.AssertFalse(err != nil && err != io.EOF, err.Error())
 }
--- build/docker.go
+++ build/docker.go
@@ -1,8 +1,8 @@
 package build
 
 import (
-	"github.com/moby/moby/api/types"
-	"github.com/moby/moby/api/types/container"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 )
 
 func (b *Build) ToMobyContainerConfig() *container.Config {
@@ -13,6 +13,6 @@ func (b *Build) ToMobyHostConfig() *container.HostConfig {
 	return &container.HostConfig{}
 }
 
-func (b *Build) ToMobyNetworkConfig() *types.NetworkConfig {
-	return &types.NetworkConfig{}
+func (b *Build) ToMobyNetworkConfig() *network.NetworkingConfig {
+	return &network.NetworkingConfig{}
 }
